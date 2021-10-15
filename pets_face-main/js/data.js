e5f691019be6af39d987d902549b853cab795a1c
Alicia Beatriz Maidana <90074289+1almamia2@users.noreply.github.com>
2021-10-15 17:10:50 -0300

    Add files via upload
    cambios

---
+++ pets_face-main/js/data.js
@@ -0,0 +1,127 @@
+const todasLasMascotas = [
+	{
+		"id": 1,
+		"image": "./imgs/cobadf" ,
+		"name": "Tatito",
+		"type": "gato",
+		"habilities": "",	
+		"hobbies": "sdasd",
+	},
+	{
+		"id": "gato",
+		"image": "./imgs/cobadf" ,
+		"name": "Milo",
+		"type": "gato",
+		"abilities": "",		
+		"hobbies": ""
+	},
+	{
+		"id": "gatita",
+		"image": "./imgs/cobadf" ,
+		"name": "jacky",
+		"type": "gato",
+		"abilities": "se victimiza",		
+		"hobbies": "dormilona",
+	},
+
+
+	{
+		"id": "gatita",
+		"image": "./imgs/cobadf",
+		"name": "Sofi",
+		"type": "gato",
+		"habilities": "solita cariño",
+		"hobbies": "dormilona",
+	},
+
+
+	{
+		"id":"gato",
+		"image": "./imgs/cobadf",
+		"name": "Tomi",
+		"type": "gato",
+		"habilities": " sentarse",
+		"hobbies": "juega con compania",
+	},
+	{
+		"id": "perro",
+		"image": "./imgs/cobadf",
+		"name": "Quien?",
+		"type": "perro",
+		"habilities": "",
+		"hobbies": "le gusta ladrar",
+	},
+
+	{
+		"id": "gatita",
+		"image": "./imgs/cobadf",
+		"name": "sally",
+		"type": "gato",
+		"habilities": " corre como gasela",
+		"hobbies": "hace pirueta",
+	},
+
+	{
+		"id": "gato",
+		"image": "./imgs/cobadf",
+		"name": "garfiel",
+		"type": "gato",
+		"habilities": " duerme sin descanso",
+		"hobbies": "tomar sol",
+	},
+
+	{
+		"id": "perrita",
+		"image": "./imgs/cobadf",
+		"name": "musi",
+		"type": "perro",
+		"habilities": "sociable  ",
+		"hobbies": "juega con compania",
+	},
+	{
+		"id": "perroblanco",
+		"image": "./imgs/cobadf",
+		"name": "musi",
+		"type": "perro",
+		"habilities": "romper papeles ",
+		"hobbies": "correr como conejo",
+	},
+
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //  SECCIÓN DE DECLARACIÓN DE INFORMACIÓN ESTÁTICA
+// let DATOSROBOTS = [{
+//   "id": 1,
+//   "nombre": "Morgan",
+//   "avatar": "https://robohash.org/nihilmodiplaceat.jpg?size=150x150&set=set1"
+// }, {
+//   "id": 2,
+//   "nombre": "Vanya",
+//   "avatar": "https://robohash.org/etitaquesunt.bmp?size=150x150&set=set1"
+// }, {
+//   "id": 3,
+//   "nombre": "Nick",
+//   "avatar": "https://robohash.org/harumdoloremqueillum.bmp?size=150x150&set=set1"
+// }, {
+//   "id": 4,
+//   "nombre": "Darcy",
+//   "avatar": "https://robohash.org/etveritatisminima.png?size=150x150&set=set1"
+// }, {
+//   "id": 5,
+//   "nombre": "Erika",
+//   "avatar": "https://robohash.org/laudantiumnequequi.png?size=150x150&set=set1"
+// }];
